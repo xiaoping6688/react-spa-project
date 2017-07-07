244d907a08571e8d9ce29c8024e0f7e17f02a118
xiaoping6688 <super_xp@126.com>
2017-07-07 16:54:22 +0800

    修复 antd-mobile svg 问题

--- src/views/Home.js
+++ src/views/Home.js
@@ -6,6 +6,7 @@ import React from 'react'
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
+import { TabBar, Icon } from 'antd-mobile'
 
 import ViewComponent from '../components/common/ViewComponent'
 import { setPageTransition } from '../stores/actions'
@@ -16,6 +17,10 @@ import '../assets/styles/Home.css'
 class Home extends ViewComponent {
   constructor (props) {
     super(props)
+    this.state = {
+      selectedTab: 'redTab',
+      hidden: false,
+    }
     this.setTransition = this.setTransition.bind(this)
   }
 
@@ -23,18 +28,122 @@ class Home extends ViewComponent {
     this.props.setPageTransition('slide-left')
   }
 
-  render() {
+  renderContent(pageText) {
     return (
-      <div className="Home">
+      <div style={{ backgroundColor: 'white', height: '100%', textAlign: 'center' }}>
         <div className="Home-header">
           <img src={logo} className="Home-logo" alt="logo" />
           <h2>Welcome To React</h2>
+          <Link to="/404" onClick={this.setTransition}>To 404</Link>
         </div>
-        <p className="Home-intro">
-          To get started, edit <code>src/*.js</code> and save to reload...
-          <Link to="/404" onClick={this.setTransition}>404</Link>
-        </p>
+
+        <div style={{ paddingTop: 60 }}>你已点击“{pageText}” tab， 当前展示“{pageText}”信息</div>
+        <a style={{ display: 'block', marginTop: 40, marginBottom: 600, color: '#108ee9' }}
+          onClick={(e) => {
+            e.preventDefault();
+            this.setState({
+              hidden: !this.state.hidden,
+            });
+          }}
+        >
+          点击切换 tab-bar 显示/隐藏
+        </a>
       </div>
+    )
+  }
+
+  render() {
+    return (
+      <TabBar
+        unselectedTintColor="#949494"
+        tintColor="#33A3F4"
+        barTintColor="white"
+        hidden={this.state.hidden}
+      >
+        <TabBar.Item
+          title="生活"
+          key="生活"
+          icon={<div style={{
+            width: '0.44rem',
+            height: '0.44rem',
+            background: 'url(https://zos.alipayobjects.com/rmsportal/sifuoDUQdAFKAVcFGROC.svg) center center /  0.42rem 0.42rem no-repeat' }}
+          />
+          }
+          selectedIcon={<div style={{
+            width: '0.44rem',
+            height: '0.44rem',
+            background: 'url(https://zos.alipayobjects.com/rmsportal/iSrlOTqrKddqbOmlvUfq.svg) center center /  0.42rem 0.42rem no-repeat' }}
+          />
+          }
+          selected={this.state.selectedTab === 'blueTab'}
+          badge={1}
+          onPress={() => {
+            this.setState({
+              selectedTab: 'blueTab',
+            });
+          }}
+          data-seed="logId"
+        >
+          {this.renderContent('生活')}
+        </TabBar.Item>
+        <TabBar.Item
+          icon={<Icon type="koubei-o" size="md" />}
+          selectedIcon={<Icon type="koubei" size="md" />}
+          title="口碑"
+          key="口碑"
+          badge={'new'}
+          selected={this.state.selectedTab === 'redTab'}
+          onPress={() => {
+            this.setState({
+              selectedTab: 'redTab',
+            });
+          }}
+          data-seed="logId1"
+        >
+          {this.renderContent('口碑')}
+        </TabBar.Item>
+        <TabBar.Item
+          icon={
+            <div style={{
+              width: '0.44rem',
+              height: '0.44rem',
+              background: 'url(https://zos.alipayobjects.com/rmsportal/psUFoAMjkCcjqtUCNPxB.svg) center center /  0.42rem 0.42rem no-repeat' }}
+            />
+          }
+          selectedIcon={
+            <div style={{
+              width: '0.44rem',
+              height: '0.44rem',
+              background: 'url(https://zos.alipayobjects.com/rmsportal/IIRLrXXrFAhXVdhMWgUI.svg) center center /  0.42rem 0.42rem no-repeat' }}
+            />
+          }
+          title="朋友"
+          key="朋友"
+          dot
+          selected={this.state.selectedTab === 'greenTab'}
+          onPress={() => {
+            this.setState({
+              selectedTab: 'greenTab',
+            });
+          }}
+        >
+          {this.renderContent('朋友')}
+        </TabBar.Item>
+        <TabBar.Item
+          icon={{ uri: 'https://zos.alipayobjects.com/rmsportal/asJMfBrNqpMMlVpeInPQ.svg' }}
+          selectedIcon={{ uri: 'https://zos.alipayobjects.com/rmsportal/gjpzzcrPMkhfEqgbYvmN.svg' }}
+          title="我的"
+          key="我的"
+          selected={this.state.selectedTab === 'yellowTab'}
+          onPress={() => {
+            this.setState({
+              selectedTab: 'yellowTab',
+            });
+          }}
+        >
+          {this.renderContent('我的')}
+        </TabBar.Item>
+      </TabBar>
     )
   }
 }
